63a3706ffb089e28fe63ce15f49b991513665b4d
DuklinDonut <majidi_fatimaezzahra@emsi-edu.ma>
2023-02-17 23:34:18 +0000

    Initial commit

---
+++ src/main/java/org/sid/ebankbankaccountservice/service/AccountService.java
@@ -0,0 +1,10 @@
+package org.sid.ebankbankaccountservice.service;
+
+import org.sid.ebankbankaccountservice.dto.BankAccountRequestDTO;
+import org.sid.ebankbankaccountservice.dto.BankAccountResponseDTO;
+
+public interface AccountService {
+    BankAccountResponseDTO addAccount(BankAccountRequestDTO bankAccountDTO);
+
+    BankAccountResponseDTO updateAccount(String id, BankAccountRequestDTO bankAccountDTO);
+}
---
+++ src/main/java/org/sid/ebankbankaccountservice/service/AccountServiceImpl.java
@@ -0,0 +1,49 @@
+package org.sid.ebankbankaccountservice.service;
+
+import org.sid.ebankbankaccountservice.dto.BankAccountRequestDTO;
+import org.sid.ebankbankaccountservice.dto.BankAccountResponseDTO;
+import org.sid.ebankbankaccountservice.entities.BankAccount;
+import org.sid.ebankbankaccountservice.mappers.AccountMapper;
+import org.sid.ebankbankaccountservice.repositories.BankAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class AccountServiceImpl implements AccountService {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    @Autowired
+    private AccountMapper accountMapper;
+    @Override
+    public BankAccountResponseDTO addAccount(BankAccountRequestDTO bankAccountDTO) {
+        BankAccount bankAccount=BankAccount.builder()
+                .id(UUID.randomUUID().toString())
+                .createdAt(new Date())
+                .balance(bankAccountDTO.getBalance())
+                .type(bankAccountDTO.getType())
+                .currency(bankAccountDTO.getCurrency())
+                .build();
+        BankAccount saveBankAccount = bankAccountRepository.save(bankAccount);
+        BankAccountResponseDTO bankAccountResponseDTO = accountMapper.fromBankAccount(saveBankAccount);
+
+        return bankAccountResponseDTO;
+    }
+    @Override
+    public BankAccountResponseDTO updateAccount(String id, BankAccountRequestDTO bankAccountDTO) {
+        BankAccount bankAccount=BankAccount.builder()
+                .id(id)
+                .createdAt(new Date())
+                .balance(bankAccountDTO.getBalance())
+                .type(bankAccountDTO.getType())
+                .currency(bankAccountDTO.getCurrency())
+                .build();
+        BankAccount saveBankAccount = bankAccountRepository.save(bankAccount);
+        BankAccountResponseDTO bankAccountResponseDTO = accountMapper.fromBankAccount(saveBankAccount);
+        return bankAccountResponseDTO;
+    }
+}
